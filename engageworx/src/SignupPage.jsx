7f97dada2d93c82603d15a949ddd76b887cdf14f
rmumby <rob@engwx.com>
2026-02-27 18:53:02 +0000

    Update SignupPage.jsx

--- engageworx/src/SignupPage.jsx
+++ engageworx/src/SignupPage.jsx
@@ -1,10 +1,5 @@
 import { useState, useEffect } from "react";
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_ANON_KEY
-);
+import { supabase } from "./supabaseClient";
 
 const PLANS = [
   {
