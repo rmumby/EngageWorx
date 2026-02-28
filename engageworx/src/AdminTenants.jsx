7d97f64a425b5fb0abdaa830e94285da29958920
rmumby <rob@engwx.com>
2026-02-28 02:07:17 +0000

    Update AdminTenants.jsx

--- engageworx/src/AdminTenants.jsx
+++ engageworx/src/AdminTenants.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { supabase } from "./supabaseClient";
-);
 
 export default function AdminTenants() {
   const [tenants, setTenants] = useState([]);
