e63507b9f434a84f78ddff7cd8562519ba8fa14b
rmumby <rob@engwx.com>
2026-03-01 02:46:47 +0000

    Update supabaseClient.js

--- engageworx/src/supabaseClient.js
+++ engageworx/src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'https://cnqasinqnjwrlfrquvbo.supabase.co';
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'sb_publishable_3oQGy2hMp_qBkw4VTok2NA_HJcS8JIt';
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNucWFzaW5xbmp3cmxmcnF1dmJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE1OTYxMDAsImV4cCI6MjA4NzE3MjEwMH0.HRou0WQWieOtHXTX_BQlEUcpQR055X-R8un3iMXrNaw';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
